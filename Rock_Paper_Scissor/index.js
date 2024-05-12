3aea17b423a39f9781082a912bab44e26df37ac5
Priyanshu Kumar <128239967+yourpriyanshu@users.noreply.github.com>
2024-05-13 01:21:47 +0530

    added objects and local storage saving in this project

--- Rock_Paper_Scissor/index.js
+++ Rock_Paper_Scissor/index.js
@@ -10,10 +10,31 @@ let winner = "";
 let c = document.querySelector("#final_result");
 c.value = winner;
 
+let score;
+let scorestr = localStorage.getItem("Score");
+if (scorestr != undefined) {
+  score = JSON.parse(scorestr);
+} else {
+  score = {
+    win: 0,
+    loss: 0,
+    draw: 0,
+  };
+}
+
+score.displayscore = function () {
+  return `Win : ${score.win}   loss : ${score.loss}  Draw : ${score.draw}`;
+};
+
 function send(value) {
   choice = value;
   a.value = choice;
+  computer();
+  final_Result();
+  Scored();
+}
 
+function computer() {
   let randomNumber = Math.random() * 3;
   if (randomNumber > 0 && randomNumber <= 1) {
     b.value = "Rock";
@@ -22,16 +43,43 @@ function send(value) {
   } else {
     b.value = "Scissor";
   }
+  // return b.value;
+}
 
+function final_Result() {
   if (a.value == b.value) {
     c.value = "Draw";
+    score.draw++;
   } else if (a.value == "Paper" && b.value == "Rock") {
     c.value = "you won";
+    score.win++;
   } else if (a.value == "Scissor" && b.value == "Paper") {
     c.value = "you won";
+    score.win++;
   } else if (a.value == "Rock" && b.value == "Scissor") {
     c.value = "you won";
+    score.win++;
   } else {
     c.value = "you loose";
+    score.loss++;
   }
+}
+
+function Scored() {
+  localStorage.setItem("Score", JSON.stringify(score));
+  let d = document.querySelector("#score");
+  d.innerText = `${score.displayscore()}`;
+}
+
+function reset() {
+  localStorage.clear();
+  score = {
+    win: 0,
+    loss: 0,
+    draw: 0,
+    displayscore: function () {
+      return `Win : ${score.win}   loss : ${score.loss}  Draw : ${score.draw}`;
+    },
+  };
+  Scored();
 }
