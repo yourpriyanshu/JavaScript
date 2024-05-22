0035d0df6cb0639ca92e6b66759f19df5c227e3f
Priyanshu Kumar <128239967+yourpriyanshu@users.noreply.github.com>
2024-05-22 11:24:57 +0530

    Completed Myntra Clone project with HTML,CSS and JavaScript

--- Myntra/index.js
+++
@@ -1,9 +0,0 @@
-let multiply = (num1, num2) => num1 * num2;
-// console.log(multiply(4, 5));
-
-let PrintFunction = (inputFunction) => {
-  let result1 = inputFunction(4, 5);
-  let result2 = inputFunction(6, 7);
-  return [result1, result2];
-};
-console.log(PrintFunction(multiply));
